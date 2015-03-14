fe2bac1f508841440a3b739994f61a592de95a08
shaunbrophy <shaunbrophy@gmail.com>
2015-03-13 20:25:34 -0400

    edits after viewing on shinyApps

--- ui.R
+++ ui.R
@@ -3,24 +3,24 @@ library(ggplot2)
 
 shinyUI(pageWithSidebar(
 
-  HTML('<div style="background-color:#dddddd;vertical-align:middle"><b><i><h2 style="color:black;text-align:center;vertical-align:middle;font:Verdana">How accurately can you estimate regression parameters?</h2></i></b></div>'),
+  HTML('<div style="background-color:#dddddd;vertical-align:middle"><b><i><h2 style="color:black;text-align:center;vertical-align:middle;font:Verdana">How accurately can you estimate the regression parameters?</h2></i></b></div>'),
 
   sidebarPanel(
     
-    sliderInput('sampleSize', 'Want to draw a different sample from the same population?', min=5, max=20, value=5, step=1, round=0),    
+    HTML('<p><b><font size="2">The plotted sample was drawn randomly from a population of 500. Try to estimate the slope and y intercept that were used to produce the population. To make it interesting, some noise was added.</font></b></p>'),
+    
+    sliderInput('sampleSize', 'If you want, draw a larger and/or  different sample from the same population.', min=5, max=20, value=5, step=1, round=0),    
    
-    sliderInput('slope', 'Estimate the slope. Use the up and down arrows to do fine tuning.', min=-15, max=15, value=0, step=0.01, round=0),
+    sliderInput('slope', 'Estimate the slope. Make fine adjustments with your keyboard arrows. Don\'t see the regression line? Your parameters just aren\'t close enough yet.', min=-15, max=15, value=0, step=0.01, round=0),
     
     sliderInput('y_intercept', 'Estimate the y intercept.', min=-25, max=25, value=0, step=0.1, round=0),
-    
-    HTML('<hr>'),
-    
-    HTML('<b><font size="2">Based on the underlying population, the parameters that you selected result in a mean squared error of</font></b>'),
+        
+    HTML('<b><font size="2">Those parameters produce a mean squared error of</font></b>'),
     textOutput('mse'),
-    
+
     HTML('<hr>'),
     
-    HTML('<b><i><font size="2">Refresh the page to generate a different distribution.</font></i></b>')
+    HTML('<b><i><font size="2">Refresh the page if you\'d like to try again with a different distribution.</font></i></b>')
   , width=4),
   
   mainPanel(
